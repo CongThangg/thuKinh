6afbb0ded78defbfa89b870785269bf0b498fcbc
Thang <luongcongthangg@gmail.com>
2022-07-12 20:56:12 +0700

    khoitao

---
+++ js/main.js
@@ -0,0 +1,67 @@
+const dataGlasses = [
+    { id: "G1", src: "./img/g1.jpg", virtualImg: "./img/v1.png", brand: "Armani Exchange", name: "Bamboo wood", color: "Brown", price: 150, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ea voluptates officiis? " },
+    { id: "G2", src: "./img/g2.jpg", virtualImg: "./img/v2.png", brand: "Arnette", name: "American flag", color: "American flag", price: 150, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. In assumenda earum eaque doloremque, tempore distinctio." },
+    { id: "G3", src: "./img/g3.jpg", virtualImg: "./img/v3.png", brand: "Burberry", name: "Belt of Hippolyte", color: "Blue", price: 100, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit." },
+    { id: "G4", src: "./img/g4.jpg", virtualImg: "./img/v4.png", brand: "Coarch", name: "Cretan Bull", color: "Red", price: 100, description: "In assumenda earum eaque doloremque, tempore distinctio." },
+    { id: "G5", src: "./img/g5.jpg", virtualImg: "./img/v5.png", brand: "D&G", name: "JOYRIDE", color: "Gold", price: 180, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error odio minima sit labore optio officia?" },
+    { id: "G6", src: "./img/g6.jpg", virtualImg: "./img/v6.png", brand: "Polo", name: "NATTY ICE", color: "Blue, White", price: 120, description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit." },
+    { id: "G7", src: "./img/g7.jpg", virtualImg: "./img/v7.png", brand: "Ralph", name: "TORTOISE", color: "Black, Yellow", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sint nobis incidunt non voluptate quibusdam." },
+    { id: "G8", src: "./img/g8.jpg", virtualImg: "./img/v8.png", brand: "Polo", name: "NATTY ICE", color: "Red, Black", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, unde enim." },
+    { id: "G9", src: "./img/g9.jpg", virtualImg: "./img/v9.png", brand: "Coarch", name: "MIDNIGHT VIXEN REMIX", color: "Blue, Black", price: 120, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur soluta ad aut laborum amet." }
+];
+
+        let getListGlasses=document.querySelector('#vglassesList')
+        let getGlassesPerson=document.querySelector('.vglasses__model')
+                                         
+        let chooseGlasses = document.querySelectorAll(".itemGlasses img")
+        let glassesInfo = document.querySelector('#glassesInfo')
+        let showGlasses="";
+
+function displayGlasses(){
+       
+        dataGlasses.map((data,id)=>{
+    
+        showGlasses+=`<div class="itemGlasses "><img onClick="chooseImg('${data.id}')" class="" src="${data.src}" id=${id}></div>`
+            
+        getListGlasses.innerHTML=showGlasses;
+
+        })
+         
+    }
+
+    displayGlasses();  
+
+
+function chooseImg(getId){
+
+        const Id= getId ;
+        let personGlasses="";
+        let showInfo="";
+
+        for(let i=0;i<dataGlasses.length;i++){
+            if(dataGlasses[i].id === Id){
+                personGlasses+=`<img class="img-glasses" src="${dataGlasses[i].virtualImg}">`
+
+                showInfo+= `<div>
+                 <p>${dataGlasses[i].name}-${dataGlasses[i].brand} (${dataGlasses[i].color})</p>
+                 <div style="margin:5px">
+                 <span class="priceGlasses">$${dataGlasses[i].price}</span><span class="text">text</span>
+                 </div>
+                 <div class="descGlasses">${dataGlasses[i].description}</div>
+                
+                
+                </div>`
+
+
+                getGlassesPerson.innerHTML=personGlasses;
+                glassesInfo.innerHTML=showInfo; 
+                glassesInfo.style.display="block";
+            }
+       }
+}
+
+
+  
+
+
+
